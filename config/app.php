809c95a905e4a7a0072b0269a7a11246cec74ed3
almasbekovnan <almasbekovna.n@gmail.com>
2026-06-04 15:40:45 +0500

    Add config/app.php

---
+++ config/app.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\ServiceProvider;
+
+return [
+
+    'name' => env('APP_NAME', 'SMS Tranche System'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => 'Asia/Almaty',
+    'locale' => 'ru',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'ru_RU',
+    'cipher' => 'AES-256-CBC',
+    'key' => env('APP_KEY'),
+    'previous_keys' => [
+        ...array_filter(
+            explode(',', env('APP_PREVIOUS_KEYS', ''))
+        ),
+    ],
+    'maintenance' => [
+        'driver' => 'file',
+    ],
+
+    'providers' => ServiceProvider::defaultProviders()->merge([
+        App\Providers\AppServiceProvider::class,
+    ])->toArray(),
+
+    'aliases' => Facade::defaultAliases()->merge([
+        //
+    ])->toArray(),
+
+];
